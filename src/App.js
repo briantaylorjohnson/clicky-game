c82685518772bb6e4fe2af5b52e53820688fc0af
Brian Taylor Johnson <brian.johnson@cigna.com>
2019-10-13 01:46:17 -0400

    Added state management to App.js and added click events to Cards component

--- src/App.js
+++ src/App.js
@@ -9,9 +9,66 @@ class App extends Component
 {
   state = 
   {
-    cards
+    cards,
+    score: 0,
+    clickedCards: [],
+    copy: "",
   };
   
+  randomizer = id =>
+  {
+    this.setState(
+    {
+      cards: this.state.cards.sort(function(a,b)
+      {
+        return 0.5 - Math.random();
+      })
+    })
+  }
+
+  gameLogic = id =>
+  {
+    let clicked = this.state.clickedCards;
+    const alreadyGuessed = clicked.includes(id);
+
+    if (!alreadyGuessed)
+    {
+      this.state.clickedCards.push(id);
+      let updatedScore = this.state.score + 1;
+      this.setState({score: updatedScore});
+
+      console.log("New Score: " + updatedScore);
+      if (updatedScore === 12)
+      {
+        this.setState(
+        {
+          score: 0,
+          alreadyGuessed: [],
+          copy: "Hooray! You are back to your new self again -- a winnner!"
+        });
+      }
+
+      else
+      {
+        this.setState(
+        {
+          copy: "That's great pick! Toad would be proud."
+        });
+      }
+    }
+
+    else
+    {
+      console.log("Game over!");
+      this.setState(
+      {
+        score: 0,
+        alreadyGuessed: [],
+        copy: "You already picked that card! Bowser's going to get the Princes! Oh no!"
+      });
+    }
+  }
+
   render()
   {
     return (
@@ -26,6 +83,9 @@ class App extends Component
                     id={cards.id}
                     name={cards.name}
                     url={cards.url}
+                    key={i}
+                    randomizer={this.randomizer}
+                    gameLogic={this.gameLogic}
                 ></Card>
                 }
                 )}
